3251aabea98446e65f22c11c1136049ac91a372a
carolinepotts <carolinepotts@msn.com>
2020-02-26 15:29:11 -0500

    Adding add and sub test cases

--- tests/test.py
+++ tests/test.py
@@ -11,6 +11,14 @@ class TestCases(unittest.TestCase):
 	Add your own test cases here
 	function name must start with test_
 	'''
+	
+	def test_addition(self):
+		self.assertEqual(5, add(2,3))
+		self.assertEqual(0, add(-1,1))
+		
+	def test_subtraction(self):
+		self.assertEqual(3, sub(5,2))
+		self.assertEqual(-4, sub(5,9))
 
 if __name__ == "__main__":
 	unittest.main()
