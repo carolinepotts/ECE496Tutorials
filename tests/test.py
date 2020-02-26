512851e87318d164cce822bb918ae7b4922c8521
carolinepotts <carolinepotts@msn.com>
2020-02-26 15:34:00 -0500

    from code import *

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,5 @@
 import unittest
-import code
+from code import *
 
 class TestCases(unittest.TestCase):
 
@@ -13,12 +13,12 @@ class TestCases(unittest.TestCase):
 	'''
 	
 	def test_addition(self):
-		self.assertEqual(5, code.add(2,3))
-		self.assertEqual(0, code.add(-1,1))
+		self.assertEqual(5, add(2,3))
+		self.assertEqual(0, add(-1,1))
 		
 	def test_subtraction(self):
-		self.assertEqual(3, code.sub(5,2))
-		self.assertEqual(-4, code.sub(5,9))
+		self.assertEqual(3, sub(5,2))
+		self.assertEqual(-4, sub(5,9))
 
 if __name__ == "__main__":
 	unittest.main()
